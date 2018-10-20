392dd35c42362f76738e3c91643ec81b1bd34ece
AlexReymer <Alexreymer@gmail.com>
2018-10-19 23:46:05 -0400

    Add leftover classes not present in previous commit. (#10)

---
+++ Skyline-Admin/src/model/DatabaseInteractor.java
@@ -0,0 +1,11 @@
+package model;
+
+public class DatabaseInteractor {
+
+    public Event getEvent(){return null;}
+
+    public Amenity getAmenity(){return null;}
+
+    public Announcement getAnnouncement(){return null;}
+
+}

c0f8e06fc33a5ff74f6100a4029c0ecaa674c2cd
Krishna Ravishankar <krishna.ravi732@gmail.com>
2018-10-26 23:59:22 -0400

    Preliminary LoginController implementation

--- Skyline-Admin/src/model/Main.java
+++ Skyline-Admin/src/model/Main.java
@@ -5,17 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import view.Controller;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/creator.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/Login.fxml"));
         primaryStage.setTitle("Skyline");
         Scene scene = new Scene(root, 400, 500);
         primaryStage.setScene(scene);
-        scene.getStylesheets().add("view/stylesheet_new.css");
+        scene.getStylesheets().add("view/StylesheetGlobal.css");
         primaryStage.show();
 
         // Testing out the lists for condos and condo managers
--- Skyline-Admin/src/view/LoginController.java
+++ Skyline-Admin/src/view/LoginController.java
@@ -5,11 +5,17 @@ import javafx.fxml.FXMLLoader;
 
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import model.DatabaseInteractor;
+import model.FirebaseAuthHandler;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class LoginController {
+    private FirebaseAuthHandler authHandler;
+    private DatabaseInteractor dbInteractor;
+
     @FXML
     private AnchorPane rootPane;
 
@@ -20,17 +26,45 @@ public class LoginController {
     private TextField password;
 
     @FXML
-    private void initialize(){}
+    private void initialize() throws IOException {
+        this.authHandler = new FirebaseAuthHandler();
+        this.dbInteractor = new DatabaseInteractor();
+    }
 
     @FXML
     private void signIn() throws IOException {
+        String uid;
+        Map<String, String> user;
+
+        try {
+            uid = this.authHandler.verifyUserAuth(username.getText(), password.getText());
+            user = this.dbInteractor.getUser(uid);
+        } catch(Exception e) {
+            System.err.println(e.toString());
+            return;
+        }
+
+        int userLevel = Integer.parseInt(user.get("level"));
+        if(userLevel == 1) {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("Index.fxml"));
+            rootPane.getChildren().setAll(pane);
+        }
+        else if(userLevel == 2) {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("Owner.fxml"));
+            rootPane.getChildren().setAll(pane);
+        }
+        else {
+            // throw a popup later
+            System.err.println("Resident account not supported");
+        }
+
         // TODO: Implement User Authentication
-        if (username.getText().equals("owner")){
+        /*if (username.getText().equals("owner")){
             AnchorPane pane = FXMLLoader.load(getClass().getResource("Owner.fxml"));
             rootPane.getChildren().setAll(pane);
         }else {
             AnchorPane pane = FXMLLoader.load(getClass().getResource("Index.fxml"));
             rootPane.getChildren().setAll(pane);
-        }
+        }*/
     }
 }

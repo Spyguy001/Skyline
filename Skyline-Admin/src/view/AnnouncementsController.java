d669c0bada45d8a52bb4a683940227c6ce50a283
AlexReymer <alexreymer@gmail.com>
2018-10-30 12:59:34 -0400

    Improve incorrect password position, improve Owner GUI, implement remove Condo/Manager functionality in GUI for owner.

--- Skyline-Admin/src/view/AnnouncementsController.java
+++ Skyline-Admin/src/view/AnnouncementsController.java
@@ -1,8 +1,51 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
 
 public class AnnouncementsController {
     @FXML
-    private void initialize(){}
+    private ListView<String> announcementList;
+
+    @FXML
+    private void initialize(){
+    }
+
+    @FXML
+    private void addAnnouncement(){
+        // TODO: Implement a dialog box with more fields
+        TextInputDialog dialog = new TextInputDialog("New Announcement");
+
+        dialog.setTitle("Add New Announcement");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Announcement Title:");
+
+        Optional<String> result = dialog.showAndWait();
+
+        result.ifPresent(title -> {
+            announcementList.getItems().add(title);
+        });
+    }
+
+    @FXML
+    private void removeAnnouncement(){
+        if (announcementList.getSelectionModel().getSelectedItem() == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please select an announcement!", ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + announcementList.getSelectionModel().getSelectedItem() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+
+            if (alert.getResult() == ButtonType.YES) {
+                announcementList.getItems().remove(announcementList.getSelectionModel().getSelectedIndex());
+            }
+        }
+    }
 }
--- Skyline-Admin/src/view/OwnerController.java
+++ Skyline-Admin/src/view/OwnerController.java
@@ -2,7 +2,9 @@ package view;
 
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 
 import javafx.beans.value.ChangeListener;
@@ -34,17 +36,60 @@ public class OwnerController {
     @FXML
     private void addManager(){
         String selected = listCondos.getSelectionModel().getSelectedItem();
-
-        if (condosToManagers.get(selected) == null) {
-            ArrayList<String> list = new ArrayList<String>();
-            list.add("Manager");
-            condosToManagers.put(selected, list);
-            listManagers.getItems().add("Manager");
-        }else{
-            condosToManagers.get(selected).add("Manager");
-            listManagers.getItems().add("Manager");
+        if (selected == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please select a Condo!", ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }else {
+            if (condosToManagers.get(selected) == null) {
+                ArrayList<String> list = new ArrayList<String>();
+                list.add("Manager");
+                condosToManagers.put(selected, list);
+                listManagers.getItems().add("Manager");
+            } else {
+                condosToManagers.get(selected).add("Manager");
+                listManagers.getItems().add("Manager");
+            }
         }
     }
+
+    @FXML
+    private void deleteManager(){
+        String selected = listManagers.getSelectionModel().getSelectedItem();
+        if (selected == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please select a manager to delete!", ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + listManagers.getSelectionModel().getSelectedItem() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+
+            if (alert.getResult() == ButtonType.YES) {
+                listManagers.getItems().remove(selected);
+                condosToManagers.get(listCondos.getSelectionModel().getSelectedItem()).remove(selected);
+            }
+        }
+    }
+
+    @FXML
+    private void deleteCondo(){
+        String selected = listCondos.getSelectionModel().getSelectedItem();
+        if (selected == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please select a condo to delete!", ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + listCondos.getSelectionModel().getSelectedItem() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+
+            if (alert.getResult() == ButtonType.YES) {
+                listCondos.getItems().remove(selected);
+            }
+        }
+    }
+
     @FXML
     private void initialize(){
 

81f06d20009ca37abe097b8687dea0fc41828165
zhan2893 <josh.zhang@mail.utoronto.ca>
2018-11-02 13:42:11 -0400

    Add all managers staff and change databaseinteractor

--- Skyline-Admin/src/model/AccountsManager.java
+++ Skyline-Admin/src/model/AccountsManager.java
@@ -1,4 +1,34 @@
+
 package model;
 
+import java.util.List;
+
 public class AccountsManager {
+	
+	  private List<ResidentAccount> reidents;
+
+
+	  public void AccountManger()  {
+
+	  }
+	  
+	  public void createResident(){
+
+		  
+	  }
+	  
+	  public void deleteResident(){
+		  
+	  }
+	  
+	  public void updateAccount() {
+		  
+	  }
+	  
+	  public void getAccounts() {
+		  
+	  }
+
 }
+
+
--- Skyline-Admin/src/model/DatabaseInteractor.java
+++ Skyline-Admin/src/model/DatabaseInteractor.java
@@ -2,21 +2,17 @@ package model;
 
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,11 +22,7 @@ public class DatabaseInteractor {
     private Firestore db;
 
     public DatabaseInteractor() throws IOException{
-			GoogleCredentials credentials;
-			File credentialsPath = new File("service_account.json");  // TODO: update to your key path.
-			try (FileInputStream serviceAccountStream = new FileInputStream(credentialsPath)) {
-				credentials = ServiceAccountCredentials.fromStream(serviceAccountStream);
-			}
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
         FirebaseOptions options = new FirebaseOptions.Builder()
             .setCredentials(credentials)
             .setProjectId("skyline-admin-219803")
@@ -128,7 +120,8 @@ public class DatabaseInteractor {
         System.out.println("Update time : " + writeResult.get().getUpdateTime());
     }
 
-    public void makeManager(String uid, String name, String condo) throws InterruptedException, ExecutionException{
+    public void makeManager(String uid, String name, String condo) 
+        throws InterruptedException, ExecutionException{
         DocumentReference docRef = db.collection("Users").document(uid);
 
         Map<String, Object> data = new HashMap<>();
@@ -147,58 +140,68 @@ public class DatabaseInteractor {
         System.out.println("Update time : " + writeResult.get().getUpdateTime());
     }
     
-    public void createAmenity(String name, String starttime, String endtime, boolean avalability)
-			  throws InterruptedException, ExecutionException{
-	        DocumentReference docRef = db.collection("Amenity").document(name);
+    public void createAmenity(String condo ,
+    	String name, String starttime, String endtime, boolean avalability)
+		throws InterruptedException, ExecutionException{
+	    DocumentReference doccondo = db.collection("Condos").document(condo);
+	    CollectionReference amenities = doccondo.collection("Amnenites");
+	    DocumentReference amenity = amenities.document(name);
 	        
-	        Map<String, Object> data = new HashMap<>();
-	        data.put("name", name);
-	        data.put("starttime", starttime);
-	        data.put("endtime", endtime);
-	        data.put("avalability", avalability);
+	    Map<String, Object> data = new HashMap<>();
+	    data.put("name", name);
+	    data.put("starttime", starttime);
+	    data.put("endtime", endtime);
+	    data.put("avalability", avalability);
 	        
-	         ApiFuture<WriteResult> result = docRef.set(data);
-	        System.out.println("Update time : " + result.get().getUpdateTime());
+	    ApiFuture<WriteResult> result = amenity.set(data);
+	    System.out.println("Update time : " + result.get().getUpdateTime());
 	  }
     
-    public void deleteAmenity(String name) throws InterruptedException, ExecutionException{
-		ApiFuture<WriteResult> writeResult = db.collection("Amenity").document("name").delete();
+    public void deleteAmenity(String condo,String name) throws InterruptedException, 
+    	ExecutionException{
+		ApiFuture<WriteResult> writeResult = db.collection("Condos").document(condo)
+		.collection("Amenities").document(name).delete();
+
 		
 		System.out.println("Update time : " + writeResult.get().getUpdateTime());
 	  }
     
-    public void updateStarttime(String name, String starttime
+    public void updateStarttime(String condo,String name, String starttime
 			  ) throws InterruptedException, ExecutionException{
-		  DocumentReference docRef = db.collection("Amenity").document(name);
-		  ApiFuture<WriteResult> resulttime = docRef.update("starttime", starttime);
+    	DocumentReference doccondo = db.collection("Condos").document(condo);
+	    CollectionReference amenities = doccondo.collection("Amnenites");
+		ApiFuture<WriteResult> resulttime = amenities.document("name").update("starttime", starttime);
 		  
-		  WriteResult result = resulttime.get();
-		  System.out.println("Write result: " + result);
+		WriteResult result = resulttime.get();
+		System.out.println("Write result: " + result);
 			
 	  }
     
-	  public void updateEndttime(String name, String Endtime
-			  ) throws InterruptedException, ExecutionException{
-		  DocumentReference docRef = db.collection("Amenity").document(name);
-		  ApiFuture<WriteResult> resulttime = docRef.update("Endtime", Endtime );
+	  public void updateEndttime(String condo,String name, String endtime
+	      ) throws InterruptedException, ExecutionException{
+	    	DocumentReference doccondo = db.collection("Condos").document(condo);
+		    CollectionReference amenities = doccondo.collection("Amnenites");
+			ApiFuture<WriteResult> resulttime = amenities.document("name").update("endtime", endtime);
 		  
 		  WriteResult result = resulttime.get();
 		  System.out.println("Write result: " + result);
 			
 	  }
 	  
-	  public void updateAvalability(String name, boolean status
-			  ) throws InterruptedException, ExecutionException{
-		  DocumentReference docRef = db.collection("Amenity").document(name);
-		  ApiFuture<WriteResult> resultstatus = docRef.update("Avalability", status);
+	  public void updateAvalability(String condo,String name, boolean status
+		  ) throws InterruptedException, ExecutionException{
+		  DocumentReference doccondo = db.collection("Condos").document(condo);
+		    CollectionReference amenities = doccondo.collection("Amnenites");
+			ApiFuture<WriteResult> resultstat = amenities.document("name").update("avalability", status);
 		  
-		  WriteResult result = resultstatus.get();
+		  WriteResult result = resultstat.get();
 		  System.out.println("Write result: " + result);
 			
 	  }
 	  
-	  public void getAmenities() throws InterruptedException, ExecutionException {
-		  ApiFuture<QuerySnapshot> query = db.collection("Amenity").get();
+	  public void getAmenities(String condo) throws InterruptedException, ExecutionException {
+		  ApiFuture<QuerySnapshot> query  = db.collection("Condos").document(condo)
+		  .collection("Amenities").get();
 		  QuerySnapshot querySnapshot = query.get();
 		  List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
 		  for (QueryDocumentSnapshot document : documents) {
@@ -209,65 +212,75 @@ public class DatabaseInteractor {
 		}
 	  }
 	  
-	  public void createAnnouncement(String title, String description, Date date ) 
-			  throws InterruptedException, ExecutionException{
-		  	DocumentReference docRef = db.collection("Announcement").document(title);
+	  public void createAnnouncement(String condo, String title, String description, String time ) 
+		  throws InterruptedException, ExecutionException{
+		  DocumentReference doccondo = db.collection("Condos").document(condo);
+		  CollectionReference announcements = doccondo.collection("Announcements");
+		  DocumentReference announcement = announcements.document(title);
 	
-	        Map<String, Object> data = new HashMap<>();
-	        data.put("title", title);
-	        data.put("description", description);
-	        data.put("Date", date);
+	      Map<String, Object> data = new HashMap<>();
+	          data.put("title", title);
+	          data.put("description", description);
+	          data.put("Time", time);
 	        
-	         ApiFuture<WriteResult> result = docRef.set(data);
-	        System.out.println("Update time : " + result.get().getUpdateTime());
+	      ApiFuture<WriteResult> result = announcement.set(data);
+	      System.out.println("Update time : " + result.get().getUpdateTime());
 	  }
 	  
-	  public void deleteAnnouncement(String title) throws InterruptedException, ExecutionException{
-		  ApiFuture<WriteResult> writeResult = db.collection("Amenity").document(title).delete();
+	  public void deleteAnnouncement(String condo,String title) 
+		  throws InterruptedException, ExecutionException{
+		  ApiFuture<WriteResult> writeResult = db.collection("Condos").document(condo)
+		  .collection("Announcements").document(title).delete();
 		  
 		  System.out.println("Update time : " + writeResult.get().getUpdateTime());
 	  }
 	  
-	  public void getAnnouncements() throws InterruptedException, ExecutionException {
-		  ApiFuture<QuerySnapshot> query = db.collection("Announcement").get();
+	  public void getAnnouncements(String condo) throws InterruptedException, ExecutionException {
+		  ApiFuture<QuerySnapshot> query  = db.collection("Condos").document(condo)
+		  .collection("Announcements").get();
 		  QuerySnapshot querySnapshot = query.get();
 		  List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
 		  for (QueryDocumentSnapshot document : documents) {
 			  System.out.println("Title: " + document.getString("title"));
 			  System.out.println("Description: " + document.getString("Description"));
-			  System.out.println("Date: " + document.getDate("Date"));
+			  System.out.println("Tate: " + document.getDate("time"));
 		}
 		  
 	  }
 	  
-	  public void createEvent(String title, String description, Date date) throws InterruptedException, 
-	  ExecutionException{
-		  	DocumentReference docRef = db.collection("Announcement").document(title);
+	  public void createEvent(String condo,String title, String description, String time) 
+	      throws InterruptedException, ExecutionException{
+		  DocumentReference doccondo = db.collection("Condos").document(condo);
+		  CollectionReference events = doccondo.collection("Events");
+		  DocumentReference event = events.document(title);
 			
-	        Map<String, Object> data = new HashMap<>();
-	        data.put("title", title);
-	        data.put("description", description);
-	        data.put("Date", date);
+	      Map<String, Object> data = new HashMap<>();
+	      data.put("title", title);
+	      data.put("description", description);
+	      data.put("time", time);
 	        
-	        ApiFuture<WriteResult> result = docRef.set(data);
-	        System.out.println("Update time : " + result.get().getUpdateTime());
+	      ApiFuture<WriteResult> result = event.set(data);
+	      System.out.println("Update time : " + result.get().getUpdateTime());
 		 
 	  }
 	  
-	  public void deleteEvent(String title) throws InterruptedException, ExecutionException{
-		   ApiFuture<WriteResult> writeResult = db.collection("Amenity").document(title).delete();
+	  public void deleteEvent(String condo,String title) 
+		  throws InterruptedException, ExecutionException{
+		  ApiFuture<WriteResult> writeResult = db.collection("Condos").document(condo)
+		  .collection("Events").document(title).delete();
 		  
 		   System.out.println("Update time : " + writeResult.get().getUpdateTime());
 	  }
 	  
-	  public void getEvents() throws InterruptedException, ExecutionException {
-		  ApiFuture<QuerySnapshot> query = db.collection("Announcement").get();
+	  public void getEvents(String condo) throws InterruptedException, ExecutionException {
+		  ApiFuture<QuerySnapshot> query  = db.collection("Condos").document(condo)
+		  .collection("Events").get();
 		  QuerySnapshot querySnapshot = query.get();
 		  List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
 		  for (QueryDocumentSnapshot document : documents) {
 			  System.out.println("Title: " + document.getString("title"));
 			  System.out.println("Description: " + document.getString("Description"));
-			  System.out.println("Date: " + document.getDate("Date"));
+			  System.out.println("Time: " + document.getDate("time"));
 		  }
 	  }
 }

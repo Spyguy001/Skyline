62550e8a6aead2979fc9cb15576fdf886c03a810
Spyguy001 <amritprabh98@hotmail.com>
2018-11-04 01:23:27 -0400

    Add method for adding/removing condos to/from residents and change get method signatures for data classes

--- Skyline-Admin/src/model/IDatabase.java
+++ Skyline-Admin/src/model/IDatabase.java
@@ -18,16 +18,18 @@ public interface IDatabase {
 
     public void addResidentToCondo(Resident resident, Condo condo);
     public void removeResidentFromCondo(Resident resident, Condo condo);
+    public void addCondoToResident(Resident resident, Condo condo);
+    public void removeCondoFromResident(Resident resident, Condo condo);
 
     public void addEventToCondo(Event event, Condo condo);
     public void removeEventFromCondo(Event event, Condo condo);
-    public List<Event> getEventsFromCondo(Condo condo);
+    public List<Event> getEventsForCondo(Condo condo);
 
     public void addAmenityToCondo(Amenity amenity, Condo condo);
     public void removeAmenityFromCondo(Amenity amenity, Condo condo);
-    public List<Amenity> getAmenitiesFromCondo(Condo condo);
+    public List<Amenity> getAmenitiesForCondo(Condo condo);
 
     public void addAnnouncementToCondo(Announcement announcement, Condo condo);
     public void removeAnnouncementFromCondo(Announcement announcement, Condo condo);
-    public List<Announcement> getAnnouncementsFromCondo(Condo condo);
+    public List<Announcement> getAnnouncementsForCondo(Condo condo);
 }

a32fc91ff26a532ff9633730b8b7d27db5c2aa4d
Krishna Ravishankar <krishna.ravi732@gmail.com>
2018-11-05 18:54:22 -0500

    Create the condo model class

--- Skyline-Admin/src/model/Condo.java
+++ Skyline-Admin/src/model/Condo.java
@@ -1,8 +1,73 @@
 package model;
 
+import java.util.List;
+
 public class Condo {
+    private String id;
+    private String name;
+    private String address;
 
-  public Condo(String name, String address){
+    private List<Event> events;
+    private List<Amenity> amenities;
+    private List<Announcement> announcements;
+    private List<String> managerIDs;
 
-  }
+    /* no args constructor required for serialize/deserialize */
+    public Condo() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    public List<Amenity> getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(List<Amenity> amenities) {
+        this.amenities = amenities;
+    }
+
+    public List<Announcement> getAnnouncements() {
+        return announcements;
+    }
+
+    public void setAnnouncements(List<Announcement> announcements) {
+        this.announcements = announcements;
+    }
+
+    public List<String> getManagerIDs() {
+        return managerIDs;
+    }
+
+    public void setManagerIDs(List<String> managerIDs) {
+        this.managerIDs = managerIDs;
+    }
 }

e307ed013e17fb62f990978d2e055ac90a5941a1
AlexReymer <alexreymer@gmail.com>
2018-11-08 13:47:44 -0500

    Add time field to Events with error checking, expand description text area.

--- Skyline-Admin/src/controller/EventsController.java
+++ Skyline-Admin/src/controller/EventsController.java
@@ -4,6 +4,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.util.Callback;
 import model.Event;
 import model.IDatabase;
 
@@ -25,7 +26,30 @@ public class EventsController {
     private TextField date;
 
     @FXML
+    private TextField time;
+
+    @FXML
+    private ChoiceBox<String> timeChoice;
+
+    @FXML
     private void initialize(){
+        //eventsTable.getColumns().forEach(this::addDescriptionTooltip);
+        // TODO: Initialize eventsTable with data from the database.
+    }
+
+    private <T> void addDescriptionTooltip(TableColumn<Event,T> column) {
+        Callback<TableColumn<Event, T>, TableCell<Event,T>> existingCellFactory
+                = column.getCellFactory();
+
+        column.setCellFactory(c -> {
+            TableCell<Event, T> cell = existingCellFactory.call(c);
+
+            Tooltip tooltip = new Tooltip();
+            tooltip.textProperty().bind(cell.itemProperty().asString());
+
+            cell.setTooltip(tooltip);
+            return cell ;
+        });
     }
 
     @FXML
@@ -39,10 +63,18 @@ public class EventsController {
                 // TODO: Add an event to the database, the following code is for testing
                 Event ev = new Event();
                 ev.setTitle(title.getText());
-                Date dateA = new SimpleDateFormat("dd/MM/yyyy").parse(date.getText());
+                String dateWithTime = date.getText() + " " + time.getText() + " " + timeChoice.getValue();
+                Date dateA = new SimpleDateFormat("dd/MM/yyyy h:mm a").parse(dateWithTime);
                 ev.setDate(dateA);
                 ev.setDescription(description.getText());
-                eventsTable.getItems().add(ev);
+                String[] hoursMinutes = time.getText().split(":");
+                if (Integer.parseInt(hoursMinutes[0]) < 1 || Integer.parseInt(hoursMinutes[0]) > 12 || Integer.parseInt(hoursMinutes[0]) < 0 || Integer.parseInt(hoursMinutes[1]) > 59){
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter a valid time!", ButtonType.CANCEL);
+                    alert.setHeaderText(null);
+                    alert.showAndWait();
+                }else {
+                    eventsTable.getItems().add(ev);
+                }
             }catch(ParseException e){
                 Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter a valid date!", ButtonType.CANCEL);
                 alert.setHeaderText(null);
@@ -59,7 +91,7 @@ public class EventsController {
             alert.showAndWait();
         }else {
             // TODO: Remove an event from the database
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + eventsTable.getSelectionModel().getSelectedItem() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + eventsTable.getSelectionModel().getSelectedItem().getTitle() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
             alert.setHeaderText(null);
             alert.showAndWait();
 
--- Skyline-Admin/src/model/Main.java
+++ Skyline-Admin/src/model/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/Login.fxml"));
         primaryStage.setTitle("Skyline");
-        Scene scene = new Scene(root, 720, 480);
+        Scene scene = new Scene(root, 1024, 576);
         primaryStage.setScene(scene);
         scene.getStylesheets().add("view/StylesheetGlobal.css");
         primaryStage.show();

0264e9824400567109780e76a61c163b0028e8c1
Krishna Ravishankar <krishna.ravi732@gmail.com>
2018-11-08 11:06:14 -0500

    Write stubs for CondoManager methods as per design draft

--- Skyline-Admin/src/model/CondoManager.java
+++ Skyline-Admin/src/model/CondoManager.java
@@ -1,4 +1,26 @@
 package model;
 
 public class CondoManager extends User {
+    private IDatabase database;
+
+    public CondoManager(IDatabase database) {
+        this.database = database;
+        this.setLevel(1);
+    }
+
+    public void addResidentToCondo(Resident resident, Condo condo) {}
+
+    public void removeResidentFromCondo(Resident resident, Condo condo) {}
+
+    public void addEventToCondo(Event event, Condo condo) {}
+
+    public void removeEventFromCondo(Event event, Condo condo) {}
+
+    public void addAmenityToCondo(Amenity amenity, Condo condo) {}
+
+    public void removeAmenityFromCondo(Amenity amenity, Condo condo) {}
+
+    public void addAnnouncementToCondo(Announcement announcement, Condo condo) {}
+
+    public void removeAnnouncementFromCondo(Announcement announcement, Condo condo) {}
 }

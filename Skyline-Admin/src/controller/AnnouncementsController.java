2944490d594a36e0b385edf6e1faefd230f3a1c2
AlexReymer <alexreymer@gmail.com>
2018-11-14 22:31:29 -0500

    Add announcements page with title, description, important fields.

--- Skyline-Admin/src/controller/AnnouncementsController.java
+++ Skyline-Admin/src/controller/AnnouncementsController.java
@@ -1,51 +1,141 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import model.Announcement;
 
-import java.util.Optional;
+import java.util.Date;
 
 public class AnnouncementsController {
     @FXML
-    private ListView<String> announcementList;
+    private TableView<Announcement> announcementsTable;
 
     @FXML
-    private void initialize(){
-    }
+    private TableColumn<Announcement, Boolean> impCol;
 
     @FXML
-    private void addAnnouncement(){
-        // TODO: Implement a dialog box with more fields
-        TextInputDialog dialog = new TextInputDialog("New Announcement");
+    private TextField title;
 
-        dialog.setTitle("Add New Announcement");
-        dialog.setHeaderText(null);
-        dialog.setContentText("Announcement Title:");
+    @FXML
+    private TextArea description;
 
-        Optional<String> result = dialog.showAndWait();
+    @FXML
+    private CheckBox important;
 
-        result.ifPresent(title -> {
-            announcementList.getItems().add(title);
+    @FXML
+    private void initialize() {
+        // TODO: Initialize announcementsTable with data from the database.
+        impCol.setCellFactory(col -> new TableCell<Announcement, Boolean>() {
+            private final ImageView imageView = new ImageView();
+
+            {
+                imageView.setFitWidth(20);
+                imageView.setFitHeight(20);
+                setGraphic(imageView);
+            }
+
+            @Override
+            protected void updateItem(Boolean item, boolean empty) {
+                if (empty || item == null) {
+                    imageView.setImage(null);
+                } else {
+                    imageView.setImage(item ? new Image("view/important-small.png") : null);
+                }
+            }
         });
     }
 
     @FXML
-    private void removeAnnouncement(){
-        if (announcementList.getSelectionModel().getSelectedItem() == null){
+    private void addAnnouncement() {
+        if (title.getText().equals("") || description.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please fill in all the required fields!", ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        } else {
+            // TODO: Add an Announcement to the database, the following code is for testing
+            Announcement an = new Announcement();
+            an.setTitle(title.getText());
+            an.setId("test");
+            //DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date date = new Date();
+            an.setDate(date);
+            an.setDescription(description.getText());
+            an.setImportant(important.isSelected());
+            announcementsTable.getItems().add(an);
+            createAnnouncementPopup();
+        }
+    }
+
+    @FXML
+    private void removeAnnouncement() {
+        if (announcementsTable.getSelectionModel().getSelectedItem() == null) {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Please select an announcement!", ButtonType.CANCEL);
             alert.setHeaderText(null);
             alert.showAndWait();
-        }else {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + announcementList.getSelectionModel().getSelectedItem() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        } else {
+            // TODO: Remove an Announcement from the database
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + announcementsTable.getSelectionModel().getSelectedItem().getTitle() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
             alert.setHeaderText(null);
             alert.showAndWait();
 
             if (alert.getResult() == ButtonType.YES) {
-                announcementList.getItems().remove(announcementList.getSelectionModel().getSelectedIndex());
+                announcementsTable.getItems().remove(announcementsTable.getSelectionModel().getSelectedItem());
             }
         }
+    }
+
+    private void createAnnouncementPopup() {
+        announcementsTable.setRowFactory(tv -> {
+            TableRow<Announcement> row = new TableRow<>();
+            row.setOnMouseClicked(e -> {
+                if (e.getClickCount() == 2 && (!row.isEmpty())) {
+                    // Create new stage for the dialog window
+                    final Stage dialog = new Stage();
+                    dialog.initModality(Modality.APPLICATION_MODAL);
+                    dialog.initOwner(announcementsTable.getScene().getWindow());
+                    Announcement an = announcementsTable.getSelectionModel().getSelectedItem();
+                    dialog.setTitle(an.getTitle());
+
+                    // Create the grid pane containing all the info
+                    GridPane gridPane = new GridPane();
+                    gridPane.setId("dialog-window");
+
+                    // Add constraints to the grid pane
+                    ColumnConstraints c1 = new ColumnConstraints();
+                    c1.setPercentWidth(25);
+                    ColumnConstraints c2 = new ColumnConstraints();
+                    c2.setPercentWidth(75);
+                    gridPane.getColumnConstraints().addAll(c1, c2);
+                    RowConstraints r1 = new RowConstraints();
+                    r1.setPercentHeight(25);
+                    RowConstraints r2 = new RowConstraints();
+                    r2.setPercentHeight(75);
+                    gridPane.getRowConstraints().addAll(r1, r2);
+
+                    // Add the info to the grid pane
+                    gridPane.add(new Label("Date:"), 0, 0);
+                    gridPane.add(new Label(an.getDate().toString()), 1, 0);
+                    gridPane.add(new Label("Description:"), 0, 1);
+                    Label desc = new Label(an.getDescription());
+                    desc.setWrapText(true);
+                    gridPane.add(desc, 1, 1);
+
+                    // Create the new scene
+                    Scene scene = new Scene(gridPane, 300, 200);
+                    scene.getStylesheets().add("view/StylesheetGlobal.css");
+                    dialog.setScene(scene);
+                    dialog.show();
+                }
+            });
+            return row;
+        });
     }
 }

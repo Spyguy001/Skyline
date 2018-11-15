bb7582b2fc54d3d9975e6070efc1baf63f5bb804
AlexReymer <alexreymer@gmail.com>
2018-11-14 20:54:04 -0500

    Add amenities page with name and details fields.

--- Skyline-Admin/src/controller/AmenitiesController.java
+++ Skyline-Admin/src/controller/AmenitiesController.java
@@ -1,51 +1,107 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import model.Amenity;
+import model.Event;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 public class AmenitiesController {
     @FXML
-    private ListView<String> amenityList;
+    private TableView<Amenity> amenitiesTable;
 
     @FXML
-    private void initialize(){
+    private TextField name;
+
+    @FXML
+    private TextArea details;
+
+    @FXML
+    private void addAmenity() {
+        if (name.getText().equals("") || details.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please fill in all the required fields!", ButtonType.CANCEL);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        } else {
+            // TODO: Add an amenity to the database, the following code is for testing
+            Amenity am = new Amenity();
+            am.setName(name.getText());
+            am.setDetails(details.getText());
+            am.setId("test");
+            amenitiesTable.getItems().add(am);
+            createAmenityPopup();
+        }
     }
 
     @FXML
-    private void addAmenity(){
-        // TODO: Implement a dialog box with more fields
-        TextInputDialog dialog = new TextInputDialog("New Amenity");
-
-        dialog.setTitle("Add New Amenity");
-        dialog.setHeaderText(null);
-        dialog.setContentText("Amenity Title:");
-
-        Optional<String> result = dialog.showAndWait();
-
-        result.ifPresent(title -> {
-            amenityList.getItems().add(title);
-        });
-    }
-
-    @FXML
-    private void removeAmenity(){
-        if (amenityList.getSelectionModel().getSelectedItem() == null){
+    private void removeAmenity() {
+        if (amenitiesTable.getSelectionModel().getSelectedItem() == null) {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Please select an amenity!", ButtonType.CANCEL);
             alert.setHeaderText(null);
             alert.showAndWait();
-        }else {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + amenityList.getSelectionModel().getSelectedItem() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        } else {
+            // TODO: Remove an event from the database
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + amenitiesTable.getSelectionModel().getSelectedItem().getName() + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
             alert.setHeaderText(null);
             alert.showAndWait();
 
             if (alert.getResult() == ButtonType.YES) {
-                amenityList.getItems().remove(amenityList.getSelectionModel().getSelectedIndex());
+                Amenity toRemove = amenitiesTable.getSelectionModel().getSelectedItem();
+                amenitiesTable.getItems().remove(toRemove);
             }
         }
+    }
+
+    private void createAmenityPopup() {
+        amenitiesTable.setRowFactory(tv -> {
+            TableRow<Amenity> row = new TableRow<>();
+            row.setOnMouseClicked(e -> {
+                if (e.getClickCount() == 2 && (!row.isEmpty())) {
+                    // Create new stage for the dialog window
+                    final Stage dialog = new Stage();
+                    dialog.initModality(Modality.APPLICATION_MODAL);
+                    dialog.initOwner(amenitiesTable.getScene().getWindow());
+                    Amenity am = amenitiesTable.getSelectionModel().getSelectedItem();
+                    dialog.setTitle(am.getName());
+
+                    // Create the grid pane containing all the info
+                    GridPane gridPane = new GridPane();
+                    gridPane.setId("dialog-window");
+
+                    // Add constraints to the grid pane
+                    ColumnConstraints c1 = new ColumnConstraints();
+                    c1.setPercentWidth(100);
+                    gridPane.getColumnConstraints().addAll(c1);
+                    RowConstraints r1 = new RowConstraints();
+                    r1.setPercentHeight(25);
+                    RowConstraints r2 = new RowConstraints();
+                    r2.setPercentHeight(75);
+                    gridPane.getRowConstraints().addAll(r1, r2);
+
+                    // Add the info to the grid pane
+                    gridPane.add(new Label("Details:"), 0, 0);
+                    Label details = new Label(am.getDetails());
+                    details.setWrapText(true);
+                    gridPane.add(details, 0, 1);
+
+                    // Create the new scene
+                    Scene scene = new Scene(gridPane, 300, 200);
+                    scene.getStylesheets().add("view/StylesheetGlobal.css");
+                    dialog.setScene(scene);
+                    dialog.show();
+                }
+            });
+            return row;
+        });
     }
 }
